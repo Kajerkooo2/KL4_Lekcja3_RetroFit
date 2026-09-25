e4dfe4361e012af34ba7971c34573b1ff944cae3
Kajerkooo2 <prokajerko79@gmail.com>
2026-09-25 10:21:07 +0200

    Zapomnialem konstruktora w Pytania

--- app/src/main/java/com/example/lkcj3mb/Pytania.java
+++ app/src/main/java/com/example/lkcj3mb/Pytania.java
@@ -7,6 +7,14 @@ public class Pytania {
     private String odpC;
     private int poprawna;
 
+    public Pytania(String tresc, String odpA, String odpB, String odpC, int poprawna) {
+        this.tresc = tresc;
+        this.odpA = odpA;
+        this.odpB = odpB;
+        this.odpC = odpC;
+        this.poprawna = poprawna;
+    }
+
     public String getTresc() { return tresc; }
     public String getOdpA() { return odpA; }
     public String getOdpB() { return odpB; }
